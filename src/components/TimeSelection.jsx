1635ce0f4ad8a83f8c4ac2ddbd5676cd3a5ebf05
ParmbeerJohal <parmbeerjohal@gmail.com>
2024-05-22 22:45:15 -0700

    UI and iteration updates

--- src/components/TimeSelection.jsx
+++ src/components/TimeSelection.jsx
@@ -2,47 +2,50 @@
 //import api from '../api/axiosConfig';
 import '../styles/timeSelection.css';
 
-//import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-//import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
-//import { getDay } from "date-fns";
 import Card from 'react-bootstrap/Card';
 
-import dateSelectionData from '../data/dateSelectionData.json';
-
 function TimeSelection() {
-  //const today = new Date();
-  //const day = getDay(today);
+
+  const times = [
+    "8:00 AM",
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+    "6:00 PM",
+    "7:00 PM",
+  ];
 
   return (
     <>
       <Card bg="light" text="dark" className="dateSelectionCard mt-2">
         <Card.Body>
           <Card.Title className="text-center">Select a time</Card.Title>
-          {Array.from({ length: 3 }).map((_, idx) => (
-              <Row key={idx} className="justify-content-center">
-              {Object.keys(dateSelectionData)
-                  .slice(idx * 4, (idx + 1) * 4) // Divide data into chunks of 4
-                  .map(key => (
-                  <Col key={key} xs={3} className="mb-2 text-center">
-                      <Button
-                        variant="dark"
-                        text="light"
-                        size="lg"
-                        className="px-3 py-2"
-                      >
-                        {dateSelectionData[key].short}
-                      </Button>
+          <Row className="justify-content-center">
+            {times.map((time, idx) => (
+                  <Col key={idx} xs={3} className="mb-2 text-center">
+                  <Button
+                    variant="dark"
+                    text="light"
+                    className="monthButton"
+                  >
+                    {time}
+                  </Button>
                   </Col>
-                  ))}
-              </Row>
-          ))}
+              ))}
+          </Row>
         </Card.Body>
       </Card>
     </>
-    );
+  );
 }
 
 export default TimeSelection;
